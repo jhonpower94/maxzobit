7d840b91283a03968993a97fc2bc4ef8f61889ab
ghost <jhonsnow751@gmail.com>
2024-05-05 11:27:04 +0100

    sfghugbfgbusfvhyfd

--- src/pages/Send.js
+++ src/pages/Send.js
@@ -122,7 +122,7 @@ const Send = () => {
       });
       setOpenSnackbar(true);
       setLoading(false);
-    } else if (selectedNetworkBalance < 1500) {
+    } else if (selectedNetworkBalance < 350) {
       setTimeout(() => {
         setValue({
           ...value,
@@ -137,7 +137,7 @@ const Send = () => {
       const newbalance = balance - value.amount;
       // add newbalance
       updateUserBalance(id, cointype, newbalance).then(() => {
-        const newNetworkBalance = selectedNetworkBalance - 1500;
+        const newNetworkBalance = selectedNetworkBalance - 350;
         // add new network balance
         updateUserBalance(id, selectedNetworkType, newNetworkBalance).then(
           () => {
