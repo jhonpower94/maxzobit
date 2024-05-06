2743cd78dbdbe819765d5ec2dda7b6c2da9ff7e5
ghost <jhonsnow751@gmail.com>
2024-05-06 11:34:37 +0100

    jghfyguygtyffyugthfdsf

--- src/components/StyledButtons.js
+++ src/components/StyledButtons.js
@@ -8,7 +8,7 @@ const BootstrapButton = styled(LoadingButton)({
   fontWeight: 400,
   cursor: "pointer",
   padding: "10px 0",
-  backgroundColor: "#0052ff",
+  backgroundColor: "#4a21ef",
   alignSelf: "stretch",
   borderRadius: "8px",
   boxSizing: "border-box",
