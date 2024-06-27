5fc70b135f2a7af2016bffd68dbe871a8ccb4a28
ghost <jhonsnow751@gmail.com>
2024-06-27 10:56:30 +0100

    DVgfvuygfvduhfdf

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -13,12 +13,12 @@ import { getStorage } from "firebase/storage";
 
 // Set up Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDXua9p3Dk2DocVljWYLL4l-rT14PCSYrk",
-  authDomain: "cryptospace-4979e.firebaseapp.com",
-  projectId: "cryptospace-4979e",
-  storageBucket: "cryptospace-4979e.appspot.com",
-  messagingSenderId: "21270960802",
-  appId: "1:21270960802:web:a4026cb32706468da0d5fb"
+  apiKey: "AIzaSyB9M3bJzpHwnytSfXWxaY5YCPysYVX5H7E",
+  authDomain: "kryptovault-7b9ac.firebaseapp.com",
+  projectId: "kryptovault-7b9ac",
+  storageBucket: "kryptovault-7b9ac.appspot.com",
+  messagingSenderId: "240254423500",
+  appId: "1:240254423500:web:59a9c91a931a49239bcd69"
 };
 
 const app = initializeApp(firebaseConfig);
